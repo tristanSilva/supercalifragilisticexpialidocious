7139dba4cd80f7da3ca177a05db43df690b3ed10
TristanSilva <tristansilva@gmail.com>
2016-02-22 20:29:24 -0800

    Added files via upload

---
+++ helloWorld.java
@@ -0,0 +1,34 @@
+public class helloWorld{
+
+public static void main(String[] args){
+
+// This is a sample hello world in a string.
+
+System.out.println("Hello World");
+
+// This is a sample hello world in a characters.
+
+char character1 = 'H';
+char character2 = 'e';
+char character3 = 'l';
+char character4 = 'l';
+char character5 = 'o';
+
+char character6 = 'W';
+char character7 = 'o';
+char character8 = 'r';
+char character9 = 'l';
+char character10 = 'd';
+
+System.out.print(character1+""+character2+""+character3+""+character4+""+character5+" ");
+System.out.println(character6+""+character7+""+character8+""+character9+""+character10);
+
+// This is a sample hello world in an array.
+
+char[] charArray ={ 'H', 'e', 'l', 'l', 'o',' ','W', 'o', 'r', 'l', 'd' }; 
+
+System.out.println(charArray);
+
+}
+
+}
