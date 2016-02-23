994c115a9573d926128535819c9d33b95daed22c
TristanSilva <tristansilva@gmail.com>
2016-02-22 20:33:40 -0800

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -6,29 +6,29 @@ public static void main(String[] args){
 
 System.out.println("Hello World");
 
-// This is a sample hello world in a characters.
+  // This is a sample hello world in a characters.
+  
+  char character1 = 'H';
+  char character2 = 'e';
+  char character3 = 'l';
+  char character4 = 'l';
+  char character5 = 'o';
+  
+  char character6 = 'W';
+  char character7 = 'o';
+  char character8 = 'r';
+  char character9 = 'l';
+  char character10 = 'd';
+  
+  System.out.print(character1+""+character2+""+character3+""+character4+""+character5+" ");
+  System.out.println(character6+""+character7+""+character8+""+character9+""+character10);
 
-char character1 = 'H';
-char character2 = 'e';
-char character3 = 'l';
-char character4 = 'l';
-char character5 = 'o';
-
-char character6 = 'W';
-char character7 = 'o';
-char character8 = 'r';
-char character9 = 'l';
-char character10 = 'd';
-
-System.out.print(character1+""+character2+""+character3+""+character4+""+character5+" ");
-System.out.println(character6+""+character7+""+character8+""+character9+""+character10);
-
-// This is a sample hello world in an array.
-
-char[] charArray ={ 'H', 'e', 'l', 'l', 'o',' ','W', 'o', 'r', 'l', 'd' }; 
-
-System.out.println(charArray);
-
-}
+  // This is a sample hello world in an array.
+  
+  char[] charArray ={ 'H', 'e', 'l', 'l', 'o',' ','W', 'o', 'r', 'l', 'd' }; 
+  
+  System.out.println(charArray);
+  
+  }
 
 }
